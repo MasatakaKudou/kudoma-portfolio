396a2289f0e0c6ebf7433f4e3c0e0c33cad8111f
kudoma-istyle <kudoma@istyle.co.jp>
2024-08-30 01:48:41 +0900

    Fix AccountLogo.

--- src/AccountLogo.tsx
+++ src/AccountLogo.tsx
@@ -10,7 +10,7 @@ interface AccountLogoProps {
 const AccountLogo: React.FC<AccountLogoProps> = ({src, url, name}) => {
   return (
     <a href={url} style={{ margin: '5px' }}>
-      <Image src={src} alt={name} height={45} />
+      <Image src={src} alt={name} fit='contain' height={45} width={45} />
     </a>
   );
 };
