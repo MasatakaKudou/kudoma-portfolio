e977c3c4080a3f8f7f6071c0445b245a317999ee
kudoma-istyle <kudoma@istyle.co.jp>
2025-02-08 00:19:51 +0900

    Fix Product.

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 
 import {
+  Box,
   Card,
   CardContent,
   CardMedia,
   CardActionArea,
+  Chip,
   Typography,
 } from '@mui/material';
 
 export interface ProductProps {
   title: string;
   description: string;
+  skills: string[];
 }
 
 export const Product: React.FC<ProductProps> = (props) => {
   return (
-    <Card sx={{ maxWidth: 345, marginTop: 2 }}>
+    <Card sx={{ maxWidth: 300, marginTop: 2 }}>
       <CardActionArea>
         <CardMedia
           component='img'
@@ -31,6 +34,11 @@ export const Product: React.FC<ProductProps> = (props) => {
           <Typography variant="body2" sx={{ color: 'text.secondary' }}>
             { props.description }
           </Typography>
+          <Box marginTop={1}>
+            {props.skills.map((skill, index) => (
+              <Chip label={skill} sx={{ marginRight: 1, marginBottom: 1 }} />
+            ))}
+          </Box>
         </CardContent>
       </CardActionArea>
     </Card>
--- src/data/products.ts
+++ src/data/products.ts
@@ -4,5 +4,12 @@ export const products: ProductProps[] = [
   {
     title: 'Kudoma Portfolio',
     description: 'このポートフォリオサイトです。',
+    skills: [
+      'React'
+      ,'TypeScript'
+      ,'Github Pages'
+      ,'Cloudflare'
+      ,'Redis'
+    ],
   },
 ];
